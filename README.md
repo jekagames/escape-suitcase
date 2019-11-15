79851968b099be3e50a72f298ad8b596e95a4076
Jess Marcotte <meaikoh@gmail.com>
2019-11-14 22:05:16 -0500

    magic patches!
    programmed the lights and vibrations for the patches!

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # escape-suitcase
-Repo for the code connected to a hybrid game where some of the clues appear on a Micro:bit and on a Raspberry Pi
+Repo for the code connected to a hybrid game where some of the clues appear on a Micro:bit and on a Raspberry Pi.
+
+CAUTION: This code contains spoilers! 
+
+Thanks to Pippin Barr for his help with the "Konami Code" being used under "Javascript Page Code". Pippin provided a working version of the code that I customized for the project's needs. 
---
+++ magic symbol code arduino/SinglePatchPurple/SinglePatchPurple.ino
@@ -0,0 +1,130 @@
+#include <Adafruit_NeoPixel.h>
+
+#include <Adafruit_DRV2605.h>
+//NEOPIXELS INITIALIZATION
+// Which pin on the Arduino is connected to the NeoPixels?
+#define PIN        6 // On Trinket or Gemma, suggest changing this to 1
+int pin         =  6; 
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS 24 // Popular NeoPixel ring size
+int numPixels   = 24; // Popular NeoPixel ring size
+int pixelFormat = NEO_GRB + NEO_KHZ800;
+Adafruit_NeoPixel *pixels;
+#define DELAYVAL 300 // Time (in milliseconds) to pause between pixels
+
+//MOTOR VIBRATION CONTROLLER
+Adafruit_DRV2605 drv;
+
+void setup() {
+  // put your setup code here, to run once:
+pixels = new Adafruit_NeoPixel(numPixels, pin, pixelFormat);
+pixels->begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+pixels->setBrightness(100);
+
+  Serial.begin(9600);//open serial port to communicate with DRV
+  Serial.println("DRV test");
+  drv.begin(); //vibration control
+  
+  drv.selectLibrary(1);
+  drv.setMode(DRV2605_MODE_INTTRIG); 
+
+
+drv.setWaveform(0, 107);  // 
+drv.setWaveform(1, 69);  // 
+drv.setWaveform(2, 119);  // 
+drv.setWaveform(3, 120);  // 
+//drv.setWaveform(4, 54);  // 
+drv.setWaveform(5, 70);  // 
+drv.setWaveform(6, 0);  // 
+
+}
+
+uint8_t effect = 0;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+drv.go();
+  Serial.print("Effect # "); Serial.println(effect);
+   pixels->clear(); // Set all pixel colors to 'off'
+   for(int i=0; i<numPixels; i++)
+ pixels->setPixelColor(i, pixels->Color(0, 100, 100));
+ pixels->setBrightness(20);
+  pixels->show();   // Send the updated pixel colors to the hardware.
+drv.go();
+delay(600);
+colorWipeFade(pixels->Color(150, 60, 120) , 20);
+pixels->setBrightness(30);
+pixels->show();
+delay(300); // Pause before next pass through loop
+pulseBrightness();
+delay(500); // Pause before next pass through loop 
+drv.go();
+delay(600);
+colorWipeFade(pixels->Color(200, 40, 150) , 20); 
+pulseBrightness();
+drv.go();
+colorWipeFade(pixels->Color(255, 100, 120) , 20);
+delay(200); 
+drv.go();
+delay(600);
+pulseBrightness();
+drv.go();
+delay(600);
+colorWipeFade(pixels->Color(75, 0, 75) , 20); 
+delay(300); 
+pixels->setBrightness(20);
+pixels->show();
+pulseBrightness();
+}
+
+void colorWipeFade(uint32_t color, int wait) {
+  for(int i=0; i<pixels->numPixels(); i++) { // For each pixel in strip...
+    pixels->setPixelColor(i, color);         //  Set pixel's color (in RAM)
+    pixels->show();                          //  Update strip to match
+    delay(wait);                           //  Pause for a moment
+  }
+}
+
+ void pulseBrightness()
+ {
+  pixels->setBrightness(30);
+pixels->show();
+delay(100); 
+pixels->setBrightness(25);
+pixels->show();
+delay(100); 
+pixels->setBrightness(20);
+pixels->show();
+delay(100); 
+pixels->setBrightness(15);
+pixels->show();
+delay(100); 
+pixels->setBrightness(10);
+pixels->show();
+delay(100); 
+pixels->setBrightness(5);
+pixels->show();
+delay(100); 
+
+pixels->setBrightness(5);
+pixels->show();
+delay(1000); // Pause before next pass through loop  
+pixels->setBrightness(5);
+pixels->show();
+delay(100); 
+pixels->setBrightness(10);
+pixels->show();
+delay(100); 
+pixels->setBrightness(15);
+pixels->show();
+delay(100); 
+pixels->setBrightness(20);
+pixels->show();
+delay(100); 
+pixels->setBrightness(25);
+pixels->show();
+delay(100); 
+pixels->setBrightness(30);
+pixels->show();
+delay(500); // Pause before next pass through loop 
+ }
